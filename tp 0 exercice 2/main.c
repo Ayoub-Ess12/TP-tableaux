9a85e40b24ce9122ebb6c3cc7a54c4fe5defb99d
Ayoub-Ess12 <158854903+Ayoub-Ess12@users.noreply.github.com>
2024-02-29 21:09:38 +0100

    main

---
+++ tp 0 exercice 2/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+    int T[7] = {18, 987, 66, 122, 77, 99, 100}, nbr , C = 0;
+
+    printf("entre la valeur recherchee :");
+    scanf("%d", &nbr);
+
+    for (int i = 0; i < 7; i++) {
+
+        if (T[i] == nbr) {
+            printf("la valeur existe, son emplacement est : %d", i);
+            C ++ ;
+            break;
+        }
+    }
+    if(C == 0){
+        printf("erreur !!");
+    }
+
+
+    return 0;
+}
